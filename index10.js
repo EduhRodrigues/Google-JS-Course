d067817a2846989fb0cf436a57989188b7698714
Jorge Eduardo Rodrigues <eduardorodrigues23@gmail.com>
2022-04-12 04:26:14 -0300

    speed checking

---
+++ index10.js
@@ -0,0 +1,8 @@
+var país = 'Canada'
+console.log(`Vivendo no ${país}`)
+if (país == 'Brasil') {
+  console.log(`Você é do Brasil`)
+} else {
+  console.log(`Você é de fora do Brasil`)
+}
+console.log = 'Use o cinto e dirija atento'
